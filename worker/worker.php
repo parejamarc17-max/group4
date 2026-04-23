bc69d3d4ef13bd9af58f1751b0d515fbff3cb0de
Jaeron2002 <jaeronjuico22@hotmail.com>
2026-04-23 22:24:02 +0800

    .

--- worker/worker.php
+++ worker/worker.php
@@ -25,7 +25,7 @@ $recent_rentals = $stmt->fetchAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <title>Worker Dashboard</title>
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/sidebar.css">
     <link rel="stylesheet" href="../assets/css/admin.css">
